3c5ad8c839795f40055338ad4bc613d8afdaeb03
Fernando Borretti <eudoxiahp@gmail.com>
2015-04-08 11:17:54 -0300

    Add source code of modal example

---
+++ modal/scripts.js
@@ -0,0 +1,44 @@
+var Modal = React.createClass({
+    render: function() {
+        if(this.props.isOpen) {
+            return (
+              <div className="modal">
+                <h3>My Modal</h3>
+                <button onClick={this.props.handleClose}>Close modal</button>
+              </div>
+            );
+        } else {
+            return null;
+        }
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function() {
+        return { isModalOpen: false };
+    },
+
+    openModal: function() {
+        this.setState({ isModalOpen: true });
+    },
+
+    closeModal: function() {
+        this.setState({ isModalOpen: false });
+    },
+
+    render: function() {
+        return (
+            <div>
+              <h1>App</h1>
+              <button onClick={this.openModal}>Open modal</button>
+              <Modal isOpen={this.state.isModalOpen}
+                     handleClose={this.closeModal} />
+            </div>
+        );
+    }
+});
+
+React.render(
+    <App/>,
+    document.body
+);
